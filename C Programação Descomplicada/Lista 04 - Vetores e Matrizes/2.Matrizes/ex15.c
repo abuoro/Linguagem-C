3d67e7eca80f0a9999f7af84b27515c9afb4e3f7
abuoro <32403207+abuoro@users.noreply.github.com>
2019-06-23 20:06:06 +0200

    Update ex15.c

--- C Programação Descomplicada/Lista 04 - Vetores e Matrizes/2.Matrizes/ex15.c
+++ C Programação Descomplicada/Lista 04 - Vetores e Matrizes/2.Matrizes/ex15.c
@@ -1,6 +1,6 @@
 /* 
-Angelo Buoro
-//INCOMPLETA
+Autor: abuoro
+//INCOMPLETO
 
 Exercicio 15: Leia uma matriz 5 x 10 que se refere respostas de 10 questoes de m ˜ ultipla escolha, referentes a 5 alunos. Leia tambem um vetor de 10 posic¸ ´ oes contendo o gabarito de respostas que podem ser a, b, c ou d. Seu programa devera comparar as respostas de cada candidato com o gabarito e emitir um vetor denominado resultado, contendo a pontuaçao correspondente a cada aluno.
 
